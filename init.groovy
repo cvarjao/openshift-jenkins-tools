dd85eb0a209e7425852cc87e37a8d09287e40e3b
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-03-08 22:43:25 -0800

    Update init.groovy

--- init.groovy
+++ init.groovy
@@ -1,6 +1,8 @@
 /* TODO:
 - Create Jenkins Credetential "Secret Text" with id "github-access-token"
 - Create Jenkins Credential "Username and Password" with id "github-account" where password is the same as "github-access-token"
+- Setup GitHub Server using the "github-access-token" credential
+  - uncheck "Manage Hooks"
 */
 
 println "Initializing from remote script"
