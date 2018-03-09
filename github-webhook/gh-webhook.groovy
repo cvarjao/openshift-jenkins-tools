e983dc416c5f6c2c1f0bbe8a5356f8b5fd32f36e
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-03-08 21:22:08 -0800

    Update gh-webhook.groovy

--- github-webhook/gh-webhook.groovy
+++ github-webhook/gh-webhook.groovy
@@ -1,2 +1,22 @@
+import hudson.model.*
+ 
+// get current thread / Executor
+def thr = Thread.currentThread()
+// get current build
+def build = thr?.executable
 
-println "Processing GitHub push payload"
+// get parameters
+def parameters = build?.actions.find{ it instanceof ParametersAction }?.parameters
+parameters.each {
+   println "parameter ${it.name}:"
+   println it.dump()
+   println "-" * 80
+}
+ 
+ 
+// ... or if you want the parameter by name ...
+def hardcoded_param = "payload"
+def resolver = build.buildVariableResolver
+def hardcoded_param_value = resolver.resolve(hardcoded_param)
+ 
+println "param ${hardcoded_param} value : ${hardcoded_param_value}"
