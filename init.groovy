f93cc475437fa48c58d58f6b1535bb2c66a82cf6
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-03-22 23:02:28 -0700

    Update init.groovy

--- init.groovy
+++ init.groovy
@@ -14,6 +14,7 @@ import com.openshift.jenkins.plugins.OpenShiftTokenCredentials;
 println "Initializing from remote script"
 
 def process=['oc', 'get', 'configmaps/__jenkins', '--template={{.data.config}}'].execute()
+process.waitFor()
 if (process.exitValue() != 0 ) throw new RuntimeException("'ConfigMap/jenkins' was NOT found (exit value:${process.exitValue()})")
 
 String jenkinsConfigText = process.text
